6107c56e645adc56ebeb731a617cfea751707ad9
walsh2am <walsh2am@mail.uc.edu>
2026-08-08 17:08:23 -0400

    EF Core to Seed Data

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -16,4 +16,55 @@ public class AppDbContext : DbContext
 
     public DbSet<Movie> Movie { get; set; }
 
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<TeamMember>().HasData(
+            new TeamMember
+            {
+                TeamMemberId = 2,
+                FullName = "Andrew Walsh",
+                Birthdate = new DateTime(2001, 5, 9),
+                CollegeProgram = "Software Application Development",
+                YearInProgram = "Freshman"
+            }
+        );
+
+        modelBuilder.Entity<Hobby>().HasData(
+            new Hobby
+            {
+                HobbyId = 6,
+                Name = "Guitar",
+                Category = "Entertainment",
+                SkillLevel = "Intermediate",
+                HoursPerWeek = 4
+            }
+        );
+
+        modelBuilder.Entity<VideoGames>().HasData(
+            new VideoGames
+            {
+                VideoGamesId = 7,
+                Name = "Minecraft",
+                Genre = "Sandbox",
+                HoursPlayed = 200,
+                isBestGame = false
+            }
+        );
+
+        modelBuilder.Entity<Movie>().HasData(
+            new Movie
+            {
+                MovieId = 6,
+                Title = "Star Wars Episode IV",
+                Genre = "Sci-fi",
+                ReleaseYear = 1977,
+                Rating = 5
+            }
+        );
+    }
+
+
 }
---
+++ Migrations/20260808210515_SeedData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinalProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Hobby",
+                columns: new[] { "HobbyId", "Category", "HoursPerWeek", "Name", "SkillLevel" },
+                values: new object[] { 6, "Entertainment", 4, "Guitar", "Intermediate" });
+
+            migrationBuilder.InsertData(
+                table: "Movie",
+                columns: new[] { "MovieId", "Genre", "Rating", "ReleaseYear", "Title" },
+                values: new object[] { 6, "Sci-fi", 5.0, 1977, "Star Wars Episode IV" });
+
+            migrationBuilder.InsertData(
+                table: "TeamMembers",
+                columns: new[] { "TeamMemberId", "Birthdate", "CollegeProgram", "FullName", "YearInProgram" },
+                values: new object[] { 2, new DateTime(2001, 5, 9, 0, 0, 0, 0, DateTimeKind.Unspecified), "Software Application Development", "Andrew Walsh", "Freshman" });
+
+            migrationBuilder.InsertData(
+                table: "VideoGames",
+                columns: new[] { "VideoGamesId", "Genre", "HoursPlayed", "Name", "isBestGame" },
+                values: new object[] { 7, "Sandbox", 200, "Minecraft", false });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hobby",
+                keyColumn: "HobbyId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "MovieId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "TeamMembers",
+                keyColumn: "TeamMemberId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "VideoGames",
+                keyColumn: "VideoGamesId",
+                keyValue: 7);
+        }
+    }
+}
