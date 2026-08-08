a800881b9860e36bba08c99846f2ff62e00c3292
walsh2am <walsh2am@mail.uc.edu>
2026-08-08 16:23:11 -0400

    NSwag Database

--- obj/Debug/net10.0/FinalProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/FinalProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cdb371b5e7c6e1c6513fcb6f6b6967a9b8bf672")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d4b91e3ecb21b8031a35b41c7cd360a7a89a3ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
